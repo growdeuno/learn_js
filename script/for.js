3f2d6dbdf8821f569431e934aa7e1cc316cee656
growdeuno <grow.deuno@gmail.com>
2018-09-21 02:24:38 +0800

    for array

--- script/for.js
+++ script/for.js
@@ -1,4 +1,4 @@
-var cryptocurrency = ["bitcoin","ether","cardano","doge","monero"];
+var cryptocurrency = ["bitcoin","cardano","doge","ether","monero"];
 
 for(i = 0; i<=4; i++){
   console.log(cryptocurrency[i]);
