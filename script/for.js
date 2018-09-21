dd3d5135c1d62e403fc4b5932db486f7265a53da
growdeuno <grow.deuno@gmail.com>
2018-09-21 11:51:25 +0800

    for array and length

--- script/for.js
+++ script/for.js
@@ -3,3 +3,11 @@ var cryptocurrency = ["bitcoin","cardano","doge","ether","monero"];
 for(i = 0; i<=4; i++){
   console.log(cryptocurrency[i]);
 }
+
+var currency = ['dollar','euro','rupiah','yen','yuan'];
+
+var total_currency = currency.length;
+console.log("Jumlah Jenis Uang "+total_currency);
+for(i = 0; i< total_currency; i++){
+  console.log(currency[i]);
+}
